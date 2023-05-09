9209e3e1f2419151fb8f0f47052e1bbabc46ad81
Marco Furio Colombo <marco.furio.colombo@gmail.com>
2023-05-09 19:45:56 +0200

    + layer for ssm - working, but to be tested further

--- modules/ssm/ssm_utilities.py
+++ modules/ssm/ssm_utilities.py
@@ -1,108 +1,148 @@
 from scipy.spatial.distance import *
-import numpy as np
 import tensorflow as tf
-
 from keras import backend as K
 from keras import layers
+import numpy as np
 
 
 def normalize_tensor01(tensor):
     # TODO: DOC
-    tensor1 = layers.subtract(
-        [tensor,
-         K.min(tensor)]
-    ),
-    tensor2 = layers.subtract(
-        [K.max(tensor),
-         K.min(tensor)]
-    )
+    min = K.min(tensor)
+    max = K.max(tensor)
+    tensor1 = tensor - min
+    tensor2 = max - min
 
     return layers.Lambda(lambda x: x[0] / x[1])([tensor1, tensor2])
 
 
-def compute_ssm(input_tensor, metric='manhattan', axis=1, p=None):
-    """
-    Computes the self similarity matrix of a given input.
-    Parameters:
-      :param input_tensor: tensor which ssm is computed upon
-      :param: (optional) float used only for minkowsy metric. See minkowsky_similarity_func(u, v, p)
-      :param axis:
-      :param metric: (optional) string indicating which distance metric to use for the ssm
-              available: 'manhattan', 'norm1', 'minkowsky', 'lpnorm'
-              non case sensitive
-      :param p: (optional) integer that needs to be used for minkwsky distance
-
-    Returns:
-      :returns ssm: bidimensional matrix (M x M) with values in  [0, 1) range
-
-    """
-
-    def manhattan_similarity_func(input1, input2):
-        """
-        Computes manhattan (L1) similarity matrix between mono dimensional tensors.
-        Parameters:
-          :param input1: first array for similarity
-          :param input2: second array for similarity
-        """
-        return K.sum(K.abs(input1 - input2), axis=axis, keepdims=True)
-
-    def minkowsky_sqrt_similarity_func(input1, input2):
-        """
-        Computes similarity matrix between mono dimensional tensors using sqrt distance.
-        Parameters:
-          :param input1: first array for similarity
-          :param input2: second array for similarity
-        """
-
-        return K.sum(K.sqrt(K.abs(input1 - input2)), axis=axis, keepdims=True)
-
-    def p_norm_similarity_func(input1, input2):
-        """
-        Computes similarity matrix between mono dimensional tensors using minkowsy distance metric (LP norm).
-        Power is determined by compute_ssm() parameters p. P MUST BE INTEGER
-
-        Parameters:
-          :param input1: first array for similarity
-          :param input2: second array for similarity
-        """
-        assert p is not None
-        return K.sum(K.pow(K.abs(input1 - input2), p), axis=axis, keepdims=True)
-
-    input_t = K.transpose(input)
-
-    metric = metric.lower()
-    print('metric: ', metric)
-
-    # self distance matrix
-    if metric == 'manhattan' or metric == 'l1norm':
-        ssm = manhattan_similarity_func(input_t, input_t)
-
-    elif metric == 'root' or metric == 'minkowsky_sqrt' or metric == 'sqrt':
-        ssm = minkowsky_sqrt_similarity_func(input_t, input_t)
-
-    elif metric == 'intMinkowsky' or metric == 'lpIntNorm':
-        ssm = p_norm_similarity_func(input_t, input_t)
-
-    else:
-        print('ERROR: metric: [', metric, '] not recongnised')
-        return -1
-
-    ssm_norm = normalize_tensor01(ssm)
-    return ssm_norm
-
-
 class SSMLayer(layers.Layer):
+
+    def compute_ssm(self, input_tensor, axis=1, p=None):
+        """
+        Computes the self similarity matrix of a given input.
+
+        Parameters:
+          :param input_tensor: tensor which ssm is computed upon
+          :param: (optional) float used only for minkowsy metric. See minkowsky_similarity_func(u, v, p)
+          :param axis:
+          :param metric: (optional) string indicating which distance metric to use for the ssm
+                  available: 'manhattan', 'norm1', 'minkowsky', 'lpnorm'
+                  non case sensitive
+          :param p: (optional) integer that needs to be used for minkwsky distance
+
+        Returns:
+          :returns ssm: bidimensional matrix (M x M) with values in  [0, 1) range
+
+        """
+
+        def manhattan_similarity_func(input1, input2):
+            """
+            Computes manhattan (L1) similarity matrix between bidimensional tensors.
+
+            Parameters:
+              :param input1: first array for similarity
+              :param input2: second array for similarity
+            """
+            distance = np.zeros((input1.shape[0], input2.shape[1]), dtype=np.float32)
+
+            for i in range(input1.shape[0]):
+                # each row is compared with each column
+                for j in range(input2.shape[1]):
+                    diff = input1[i, :] - input2[:, j]
+                    distance[i][j] = K.sum(K.abs(diff), keepdims=False)
+
+            distance = K.constant(distance,
+                                  dtype="float32",
+                                  shape=(input1.shape[0], input2.shape[1]))
+            return distance
+
+        def minkowsky_sqrt_similarity_func(input1, input2):
+            """
+            Computes similarity matrix between bidimensional tensors using sqrt distance.
+
+            Parameters:
+              :param input1: first array for similarity
+              :param input2: second array for similarity
+            """
+
+            distance = np.zeros((input1.shape[0], input2.shape[1]), dtype=np.float32)
+
+            for i in range(input1.shape[0]):
+                # each row is compared with each column
+                for j in range(input2.shape[1]):
+                    diff = input1[i, :] - input2[:, j]
+                    distance[i][j] = K.sum(K.sqrt(K.abs(diff)), keepdims=False)
+
+            distance = K.constant(distance,
+                                  dtype="float32",
+                                  shape=(input1.shape[0], input2.shape[1]))
+            return distance
+
+        def p_norm_similarity_func(input1, input2):
+            """
+            Computes similarity matrix between bidimensional tensors using minkowsy distance metric (LP norm).
+            Power is determined by compute_ssm() parameters p.
+            Parameters:
+              :param input1: first array for similarity
+              :param input2: second array for similarity
+            """
+            assert p is int
+            distance = np.zeros((input1.shape[0], input2.shape[1]), dtype=np.float32)
+
+            for i in range(input1.shape[0]):
+                # each row is compared with each column
+                for j in range(input2.shape[1]):
+                    diff = input1[i, :] - input2[:, j]
+                    distance[i][j] = K.sum(K.pow(K.abs(diff), p), keepdims=False)
+
+            distance = K.constant(distance,
+                                  dtype="float32",
+                                  shape=(input1.shape[0], input2.shape[1]))
+            return distance
+
+        def prod_similarity_func(input1, input2):
+            """
+            Computes dot similarity between two input matricees
+
+            Parameters:
+              :param input1: first array for similarity
+              :param input2: second array for similarity
+            """
+            return K.dot(input1, input2)
+
+        input_tensor_t = K.transpose(input_tensor)
+        print('metric: ', self.metric)
+
+        # self distance matrix
+        if self.metric == 'manhattan' or self.metric == 'l1norm':
+            dist_matrix = manhattan_similarity_func(input_tensor_t, input_tensor)
+
+        elif self.metric == 'root' or self.metric == 'minkowsky_sqrt' or self.metric == 'sqrt':
+            dist_matrix = minkowsky_sqrt_similarity_func(input_tensor, input_tensor_t)
+
+        elif self.metric == 'intMinkowsky' or self.metric == 'lpIntNorm':
+            dist_matrix = p_norm_similarity_func(input_tensor, input_tensor_t)
+
+        elif self.metric == 'dot' or self.metric == 'multiplication' or self.metric == 'dotNorm':
+            dist_matrix = prod_similarity_func(input_tensor, input_tensor_t)
+
+        else:
+            print('ERROR: metric: [', self.metric, '] not recongnised')
+            return -1
+
+        dist_matrix_norm = normalize_tensor01(dist_matrix)
+        ssm = 1 - dist_matrix_norm
+        return ssm
 
     def __init__(self, metric='manhattan', p=0.5):
         # TODO: doc
         super(SSMLayer, self).__init__()
+        self.metric = metric.lower()
 
     def call(self, inputs, *args, **kwargs):
         # TODO: doc
-        ssm = compute_ssm(inputs)
+        ssm = self.compute_ssm(inputs)
         return ssm
-
-
 # ======================== Equivalent np array operations =========================
 
 def compute_matrix_ssm(piano_roll_input, metric='manhattan', p=0.5):
