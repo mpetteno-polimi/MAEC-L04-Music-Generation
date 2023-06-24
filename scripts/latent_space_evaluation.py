fb32531c5d1ed8f01b80343f0df1b77db42029c9
Matteo Pettenò <matteo.petteno@mail.polimi.it>
2023-06-24 18:24:50 +0200

    - Useless script

--- scripts/latent_space_evaluation.py
+++ scripts/latent_space_evaluation.py
@@ -3,6 +3,9 @@
 import os
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
+from scipy.stats import pearsonr
 
 from definitions import ConfigSections
 from modules.utilities import config as config_file
@@ -11,14 +14,7 @@ logging = tf.compat.v1.logging
 script_config = config_file.load_configuration_section(ConfigSections.LATENT_SPACE_SAMPLING)
 
 
-def run():
-    output_dir = os.path.expanduser(script_config.get("output_dir"))
-    model_config = script_config.get("model_config")
-    rand_seed = script_config.get("rand_seed")
-
-    run_folder_name = 'config_%s_seed_%d' % (model_config, rand_seed)
-    run_folder_path = os.path.join(output_dir, run_folder_name)
-
+def load_matrices(run_folder_path):
     grid_points_coordinates = []
     sample_coordinates = []
     sample_complexities = []
@@ -54,13 +50,83 @@ def run():
         sample_coordinates.append(grid_point_samples_coord)
         sample_complexities.append(grid_point_samples_complexities)
 
-    grid_points_coordinates = np.asarray(grid_points_coordinates)
-    sample_coordinates = np.asarray(sample_coordinates)
-    sample_complexities = np.asarray(sample_complexities)
+    return np.asarray(grid_points_coordinates), np.asarray(sample_coordinates), np.asarray(sample_complexities)
 
-    np.save(os.path.join(output_dir, 'grid_points_coordinates'), grid_points_coordinates)
-    np.save(os.path.join(output_dir, 'sample_coordinates'), sample_coordinates)
-    np.save(os.path.join(output_dir, 'sample_complexities'), sample_complexities)
+
+def evaluate(grid_points_coordinates, sample_coordinates, sample_complexities):
+    correlation(sample_coordinates, sample_complexities)
+    multi_dimensional_scaling(sample_coordinates, sample_complexities)
+
+
+def correlation(sample_coordinates, sample_complexities):
+    # TODO - Probably there's a quicker way to do this
+    # flatten to 2 dim
+    coordinates = sample_coordinates.reshape(-1, sample_coordinates.shape[-1])
+    coordinates = np.transpose(coordinates, (1, 0))
+    complexities = sample_complexities.reshape(-1, sample_complexities.shape[-1])
+    complexities = np.transpose(complexities, (1, 0))
+
+    # for each dimension compute pearson correlation and store then in a array
+    pearson_correlations = []
+    for complexity in complexities:
+        correlations = []
+        for coord in coordinates:
+            corr = pearsonr(coord, complexity)
+            correlations.append(corr)
+        pearson_correlations.append(correlations)
+
+    # TODO - Do something with correlation coefficients
+
+    return np.asarray(pearson_correlations)
+
+
+def multi_dimensional_scaling(sample_coordinates, sample_complexities):
+    # TODO - Review
+    # flatten to 2 dim
+    sample_coordinates_unrolled = sample_coordinates.reshape(-1, sample_coordinates.shape[-1])
+    sample_complexities = sample_complexities.reshape(-1, sample_complexities.shape[-1])
+
+    sample_complexities_norm = sample_complexities[:, 0]
+    # sample_complexities_norm = (sample_complexities_norm - np.min(sample_complexities_norm)) /
+    # (np.max(sample_complexities_norm) - np.min(sample_complexities_norm))
+
+    multi_dim_scaler = MDS(n_components=2, verbose=2, max_iter=100)
+    scaled_sample_coordinates = multi_dim_scaler.fit_transform(sample_coordinates_unrolled[0:100])
+
+    print('sample_coordinates_2d.shape', sample_coordinates.shape)
+    print('scaled_sample_coordinates', scaled_sample_coordinates.shape)
+    print('scaled_sample_coordinates max', np.min(scaled_sample_coordinates))
+    print('scaled_sample_coordinates min', np.min(scaled_sample_coordinates))
+    print('sample_complexities_norm', sample_complexities_norm.shape)
+    print('sample_complexities_norm max', np.max(sample_complexities_norm))
+    print('sample_complexities_norm min', np.min(sample_complexities_norm))
+
+    plt.figure(0)
+    fig, ax = plt.subplots()
+
+    ax.scatter(scaled_sample_coordinates[0:100, 0], scaled_sample_coordinates[0:100, 1],
+               c=sample_complexities_norm[0:100], s=sample_complexities_norm[0:100], vmin=0, vmax=50)
+    ax.set(xlim=(-100, 100), xticks=np.arange(-100, 100),
+           ylim=(-100, 100), yticks=np.arange(-100, 100))
+
+    plt.show()
+
+
+def run():
+    output_dir = os.path.expanduser(script_config.get("output_dir"))
+    model_config = script_config.get("model_config")
+    rand_seed = script_config.get("rand_seed")
+    run_folder_name = 'config_%s_seed_%d' % (model_config, rand_seed)
+    run_folder_path = os.path.join(output_dir, run_folder_name)
+
+    grid_points_coordinates, sample_coordinates, sample_complexities = load_matrices(run_folder_path)
+
+    # Save matrices (for sharing purposes)
+    np.save(os.path.join(run_folder_path, 'grid_points_coordinates'), grid_points_coordinates)
+    np.save(os.path.join(run_folder_path, 'sample_coordinates'), sample_coordinates)
+    np.save(os.path.join(run_folder_path, 'sample_complexities'), sample_complexities)
+
+    evaluate(grid_points_coordinates, sample_coordinates, sample_complexities)
 
 
 def main(_):
