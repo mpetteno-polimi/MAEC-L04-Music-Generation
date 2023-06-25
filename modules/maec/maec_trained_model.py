[FIX] Empty MIDI files + new computation for sigma [FIX]: Toussant complexity bug

modules/maec/maec_trained_model.py
@@ -1,10 +1,11 @@
 
 import numpy as np
+import tensorflow as tf
 
 from magenta.models.music_vae import TrainedModel
 
 from definitions import ConfigSections
-from modules.utilities import sampling, math
+from modules.utilities import sampling
 from modules.utilities import config as config_file
 
 
@@ -20,12 +21,13 @@ class MAECTrainedModel(TrainedModel):
         self._grid_width = self._config_file.get("grid_width")
         self._rand_seed = self._config_file.get("rand_seed")
 
-    def grid_sample(self, n_grid_points, n_samples_per_grid_point, length=None, temperature=1.0):
+    def grid_sample(self, n_grid_points, n_samples_per_grid_point, k_sigma=3, length=None, temperature=1.0):
         """ TODO
 
         Args:
           n_grid_points: .
           n_samples_per_grid_point: .
+          k_sigma: .
           length: The maximum length of a sample in decoder iterations. Required
             if end tokens are not being used.
           temperature: The softmax temperature to use (if applicable).
@@ -48,15 +50,20 @@ class MAECTrainedModel(TrainedModel):
             rand_seed=self._rand_seed
         )
 
+        sigma, min_point_distance = sampling.get_sigma_from_grid_points(z_grid, k_sigma)
+        tf.compat.v1.logging.info("Minimum distance between grid points is %s" % min_point_distance)
+        tf.compat.v1.logging.info("Setting sigma to %s" % sigma)
+
+        tf.compat.v1.logging.info("Performing gaussian sampling...")
         batched_gaussian_samples = sampling.batch_gaussian_sampling(
             d=self._config.hparams.z_size,
             grid_points=z_grid,
             samples_per_point=n_samples_per_grid_point,
-            sigma=1,
+            sigma=sigma,
             rand_seed=self._rand_seed
         )
 
-        # Decode samples
+        tf.compat.v1.logging.info("Decoding samples...")
         outputs = []
         for idx in range(n_grid_points):
             feed_dict[self._z_input] = batched_gaussian_samples[:, idx, :]

modules/utilities/complexity_measures.py
@@ -4,7 +4,8 @@ import pretty_midi
 
 def sanitize(midi: pretty_midi.PrettyMIDI, bars: int):
     assert len(midi.instruments) == 1, \
-        f'MusicVAE output is expected to have a single MIDI instrument. Found ({len(midi.instruments)}) instruments instead'
+        f'MusicVAE output is expected to have a single MIDI instrument. Found ({len(midi.instruments)})' \
+        f'instruments instead'
 
     for instrument in midi.instruments:
         instrument.pitch_bends = []
@@ -60,7 +61,7 @@ def toussaint(midi: pretty_midi.PrettyMIDI, bars: int = 16, binary: bool = True)
                 break
 
     metricity = np.sum(hierarchy * velocity)
-    metric = max_sum[n_onsets] - metricity
+    metric = max_sum[n_onsets-1] - metricity
 
     return metric
 

modules/utilities/sampling.py
@@ -1,10 +1,10 @@
 
 import numpy as np
+from scipy.spatial.distance import cdist
 from scipy.stats import qmc
 
 
 def latin_hypercube_sampling(d, grid_width, n_grid_points, rand_seed):
-    # TODO: change grid_width with percentile
     grid_min = int(-grid_width / 2)
     grid_min = np.full(shape=d, fill_value=grid_min)
     grid_max = int(grid_width / 2)
@@ -25,3 +25,12 @@ def batch_gaussian_sampling(d, grid_points, samples_per_point, sigma, rand_seed)
         batched_samples[:, idx, :] = samples
 
     return np.asarray(batched_samples)
+
+
+def get_sigma_from_grid_points(grid_points, k_sigma):
+    dm = cdist(grid_points, grid_points, metric='euclidean')
+    dm = dm[dm > 0]
+    dist_min = np.amin(dm)
+    half_dist_min = dist_min / 2
+    chosen_sigma = half_dist_min / k_sigma
+    return chosen_sigma, dist_min

scripts/latent_space_complexities.py
@@ -26,10 +26,16 @@ def run():
     samples_folder_name = 'samples'
     samples_folder_path = os.path.join(output_dir, run_folder_name, samples_folder_name)
     file_name_pattern = samples_folder_path + "/**/*.mid"
+    empty_midi_file_count = 0
     for idx, file_path in enumerate(glob.glob(file_name_pattern, recursive=True)):
         # Load MIDI file
         midi_file = pretty_midi.PrettyMIDI(file_path)
-        midi_file = complexity_measures.sanitize(midi_file, num_bars)
+        try:
+            midi_file = complexity_measures.sanitize(midi_file, num_bars)
+        except AssertionError as e:
+            logging.warning("Skipping MIDI file %s. Cause is: %s" % (file_path, e.args))
+            empty_midi_file_count = empty_midi_file_count + 1
+            continue
 
         # Complexities computation
         note_density = complexity_measures.note_density(midi_file, num_bars, binary=True)
@@ -45,6 +51,8 @@ def run():
         sample_complexities_file_path = os.path.join(folder_path, sample_complexities_file_name)
         sample_complexities = [toussaint, note_density, pitch_range, contour]
         np.save(sample_complexities_file_path, sample_complexities)
+
+    logging.info("Found %d empty MIDI files." % empty_midi_file_count)
 
 
 def main(_):

scripts/latent_space_evaluation.py
@@ -42,8 +42,11 @@ def load_matrices(grid_folder_path):
                 # Load sample complexities
                 sample_complexities_file_name = 'sample_complexities_%s.npy' % sample_folder_idx
                 sample_complexity_file_path = os.path.join(sample_folder_path, sample_complexities_file_name)
-                sample_complexity = np.load(sample_complexity_file_path)
-                grid_point_samples_complexities.append(sample_complexity)
+                if os.path.isfile(sample_complexity_file_path):
+                    sample_complexity = np.load(sample_complexity_file_path)
+                    grid_point_samples_complexities.append(sample_complexity)
+                else:
+                    grid_point_samples_complexities.append([np.nan, np.nan, np.nan, np.nan])
 
                 # Load sample coordinates
                 sample_coord_file_name = 'sample_coord_%s.npy' % sample_folder_idx
@@ -81,11 +84,15 @@ def correlation(output_dir, grid_points_coordinates, sample_coordinates, sample_
     correlation_folder_path = os.path.join(output_dir, 'correlation')
     tf.compat.v1.gfile.MakeDirs(correlation_folder_path)
     n_dimension = sample_coordinates.shape[2]
-    sample_complexities_mean = sample_complexities.mean(1)
+    sample_complexities_mean = np.nanmean(sample_complexities, axis=1)
 
     # Flatten first two dimensions
     flatten_coordinates = sample_coordinates.reshape(-1, n_dimension)
     flatten_complexities = sample_complexities.reshape(-1, sample_complexities.shape[2])
+
+    # Filter NaN values from flatten arrays
+    flatten_coordinates = flatten_coordinates[~np.isnan(flatten_complexities).any(axis=1), :]
+    flatten_complexities = flatten_complexities[~np.isnan(flatten_complexities).any(axis=1), :]
 
     # Plot correlation between all complexities methods
     cmat = np.corrcoef(flatten_complexities.T)